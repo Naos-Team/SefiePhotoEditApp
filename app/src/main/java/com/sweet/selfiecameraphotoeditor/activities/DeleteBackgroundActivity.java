87169972244c3050a5666cf050b69f9c5d0edcc9
yennhiluu2112 <yennhiluu2112@gmail.com>
2022-07-12 10:42:39 +0700

    nhi cm

---
+++ app/src/main/java/com/sweet/selfiecameraphotoeditor/activities/DeleteBackgroundActivity.java
@@ -0,0 +1,16 @@
+package com.sweet.selfiecameraphotoeditor.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sweet.selfiecameraphotoeditor.R;
+
+public class DeleteBackgroundActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete_background);
+    }
+}
